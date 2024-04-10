a00b6a212d43ab3a23f977b8dd7e84e571f35f0c
Brian <chumobrian@gmail.com>
2024-04-10 10:27:40 +0300

    feat: Add a function that calls a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error(''));
-    }
-  })
-    .then(() => { console.log('Got a response from the API'); });
+  return promise
+    .then(() => { console.log({ status: 200, body: 'Success' }); })
+    .then(() => { console.log('Got a response from the API'); })
+    .catch(new Error());
 }
