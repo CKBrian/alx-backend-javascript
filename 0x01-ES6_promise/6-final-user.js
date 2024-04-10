1520b2b97e32dde3a30ad3c567623883878a0cb7
Brian <chumobrian@gmail.com>
2024-04-10 18:31:34 +0300

    feat: Add a function that settles all promises and returns an array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((resp) => (
-      resp.map((res) => ({
-        status: res.status,
-        value: res.status === 'fulfilled' ? res.value : res.reason,
-      }))
-    ));
+    .then((resp) => {
+      const profile = resp.map((res) => (
+        {
+          status: res.status,
+          value: res.status === 'fulfilled' ? res.value : String(res.reason),
+        }));
+      // console.log(profile)
+      return profile;
+    });
 }
