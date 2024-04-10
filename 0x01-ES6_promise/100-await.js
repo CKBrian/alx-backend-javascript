636751be4285eba8336259f6b18044379c81337e
Brian <chumobrian@gmail.com>
2024-04-10 20:28:27 +0300

    feat: Add a function that calls an async function and return output in a dict object

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    return Promise
+      .all([uploadPhoto(), createUser()])
+      .then((resp) => {
+        const [photo, user] = resp;
+        return {
+          photo,
+          user,
+        };
+      });
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
