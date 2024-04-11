05ff9df6a36a6086fdef6d13462f70831e615a3d
Brian <chumobrian@gmail.com>
2024-04-11 16:32:16 +0300

    feat: Add a function that creates and implement a class named ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
