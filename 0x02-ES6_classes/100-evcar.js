e63dcca5637f101aaa64f1bd3dfb42a00639a764
Brian <chumobrian@gmail.com>
2024-04-12 05:59:01 +0300

    feat: Add a script that creates and implement a class named EVCars tha extends Cars

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() { return this._range; }
+
+  set range(value) {
+    if (typeof (value) !== 'string') { throw new TypeError('Range must be a string'); }
+    this._range = value;
+  }
+
+  cloneCar() {
+    const CarClone = super.constructor[Symbol.species];
+    return new CarClone();
+  }
+}
