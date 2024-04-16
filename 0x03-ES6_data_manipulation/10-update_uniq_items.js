086856dbf2df607434022a2ccfb70d5037b8b54b
Brian <chumobrian@gmail.com>
2024-04-16 22:17:43 +0300

    feat: Add a script that creates a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,6 @@
 export default function updateUniqueItems(items) {
   if (!(items instanceof Map)) { throw new Error('Cannot process'); }
-  return items
-    .forEach((value, key, map) => {
+  items.forEach((value, key, map) => {
       if (value === 1) {
         map.set(key, 100);
       }
