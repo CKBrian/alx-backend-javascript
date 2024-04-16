78bca40b0ebe234342b710fb1d2df96d65734267
Brian <chumobrian@gmail.com>
2024-04-16 21:56:54 +0300

    feat: Add a script that creates and exports a function and a WeakMap object

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,11 @@
 const weakMap = new WeakMap();
-export const weakMap
-export default function queryAPI (endpoint){
-  if (!weakMap.has(endpoint)){weakMap.set(endpoint, 0)}
-	if(weakMap.get(endpoint) >= 5){ throw new Error("Endpoint load is high.")}
-	weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) { weakMap.set(endpoint, 0); }
+  if (weakMap.get(endpoint) >= 5) { throw new Error('Endpoint load is high.'); }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
+// Export the const instance of WeakMap
+export { weakMap };
+
+// Export the queryAPI function
+export { queryAPI };
