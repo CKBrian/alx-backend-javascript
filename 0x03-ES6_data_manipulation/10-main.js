07d8f8749c72b268239322d39af33637a3bf126d
Brian <chumobrian@gmail.com>
2024-04-17 09:25:12 +0300

    chore: Add main.js script for testing a function in the 0x03-ES6_data_manipulation project

--- 0x03-ES6_data_manipulation/10-main.js
+++ 0x03-ES6_data_manipulation/10-main.js
@@ -5,21 +5,6 @@ const map = groceriesList();
 console.log(map);
 
 updateUniqueItems(map)
+updateUniqueItems(["helo"])
 console.log(map);
 
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 10-main.js 
-Map {
-  'Apples' => 10,
-  'Tomatoes' => 10,
-  'Pasta' => 1,
-  'Rice' => 1,
-  'Banana' => 5
-}
-Map {
-  'Apples' => 10,
-  'Tomatoes' => 10,
-  'Pasta' => 100,
-  'Rice' => 100,
-  'Banana' => 5
-}
--- 0x03-ES6_data_manipulation/4-main.js
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -5,23 +5,3 @@ console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ stud
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
 
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 4-main.js 
-[
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-    grade: 86
-  },
-  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
-]
-[
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-    grade: 'N/A'
-  },
-  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
-]
