ed0871f1f077cf1f3dc036b7c576b211d89c3426
Brian <chumobrian@gmail.com>
2024-06-04 21:48:34 +0300

    Feat: Add a program that is executed through command line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,9 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-/**
- * A function to display a message to the console.
- *
- * @param {string} message - The message to be displayed.
- */
-const displayMessage = (message) => {
-  process.stdout.write(message + '\n');
-};
-const messages = [
-  'Welcome to Holberton School, what is your name?',
-  'Your name is:',
-  'This important software is now closing'
-];
-
-displayMessage(messages[0]);
 process.stdin.on('data', (data) => {
-  displayMessage(`${messages[1]} ${data.toString().trim()}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.on('SIGINT', () => {
-  displayMessage(messages[2]);
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
