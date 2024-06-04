87d01f96946168a1faad5162b737dec16e033d9d
Brian <chumobrian@gmail.com>
2024-06-05 01:00:31 +0300

    Feat: Add a program that reads from a file stream

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const students = [];
+    const fields = new Set();
+
+    const data = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
+    const lines = data.split('\n').filter((line, idx) => { return idx !== 0 && line.length !== 0; });
+    lines.forEach((line) => {
+      students.push(line);
+      fields.add(line.split(',')[3]);
+    });
+    console.log(`Number of students: ${students.length}`);
+    fields.forEach(field => {
+      const fieldList = [];
+      students.forEach(student => {
+        if (student.split(',')[3] === field) {
+          fieldList.push(student.split(',')[0]);
+        }
+      });
+      console.log(`Number of students in ${field}: ${fieldList.length}. List: ${fieldList}`);
+    });
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
