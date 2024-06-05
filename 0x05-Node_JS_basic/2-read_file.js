7de12f86e8f7deaea2d6769d74ff08a6881be9da
Brian <chumobrian@gmail.com>
2024-06-05 11:21:04 +0000

    Feat: Add a program that reads from a file using synchronou functions

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,31 @@
+const fs = require('fs');
+
 const countStudents = (path) => {
   try {
     const students = [];
     const fields = new Set();
 
-    fs.existsSync(path, exists => {
+    fs.existsSync(path, (exists) => {
       if (!exists) {
         throw new Error('Cannot load the database');
       }
     });
     const data = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
-    const lines = data
-      .split('\n')
-      .filter((line, idx) => idx !== 0 && line.length !== 0);
+    const lines = data.split('\n').filter((line, idx) => { return idx !== 0 && line.length !== 0; });
     lines.forEach((line) => {
       students.push(line);
       fields.add(line.split(',')[3]);
     });
     console.log(`Number of students: ${students.length}`);
-    fields.forEach((field) => {
+    fields.forEach(field => {
       const fieldList = [];
-      students.forEach((student) => {
+      students.forEach(student => {
         if (student.split(',')[3] === field) {
           fieldList.push(student.split(',')[0]);
         }
       });
       const names = fieldList.join(', ');
-      console.log(
-        `Number of students in ${field}: ${fieldList.length}. List: ${names}`
-      );
+      console.log(`Number of students in ${field}: ${fieldList.length}. List: ${names}`);
     });
   } catch (err) {
     console.error('Cannot load the database');
@@ -36,4 +34,3 @@ const countStudents = (path) => {
 };
 
 module.exports = countStudents;
-
