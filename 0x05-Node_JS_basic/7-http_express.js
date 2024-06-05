eccd2b22436f83c5b32900d8d2ad16699a8711b9
Brian <chumobrian@gmail.com>
2024-06-05 21:47:56 +0000

    feat: Add a program that creates a  live server tha listens on port 1245

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  countStudents('database.csv').then((data) => {
+    res.send(`This is the list of our students\n${data}`);
+  }).catch((err) => {
+    res.send(`This is the list of our students\n${err.message}`);
+  });
+});
+app.listen(1245);
+
+module.exports = app;
