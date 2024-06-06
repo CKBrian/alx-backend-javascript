a7305e2869fc1ac5e36f7da0e80abc4f33f83bc3
Brian <chumobrian@gmail.com>
2024-06-06 21:42:47 +0300

    Feat: add calculateNumber function unittest cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,18 +2,18 @@ const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
 it('should add two numbers', () => {
-  let res = calculateNumber(1, 3);
+  const res = calculateNumber(1, 3);
   assert.strictEqual(res, 4);
 });
 it('should add two numbers', () => {
-  let res = calculateNumber(1, 3.7);
+  const res = calculateNumber(1, 3.7);
   assert.strictEqual(res, 5);
 });
 it('should add two numbers', () => {
-  let res = calculateNumber(1.2, 3.7);
+  const res = calculateNumber(1.2, 3.7);
   assert.strictEqual(res, 5);
 });
 it('should add two numbers', () => {
-  let res = calculateNumber(1.5, 3.7);
+  const res = calculateNumber(1.5, 3.7);
   assert.strictEqual(res, 6);
 });
