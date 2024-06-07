7b1681f4f064096588d20ec0a0e5256e5998c145
Brian <chumobrian@gmail.com>
2024-06-07 23:36:24 +0300

    feat: Add a test suite for the getPaymentTokenFromAPI method

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,19 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const getPaymentTokenFromAPI = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token_0');
+
 
 describe('getPaymentTokenFromAPI', () => {
+  let spyAsync;
+
   beforeEach( () => {
     spyAsync = sinon.spy();
   });
+  afterEach( () => {
+    sinon.restore();
+  });
+
+
 
   it('should test an async function', (done) => {
     getPaymentTokenFromAPI(true).then( (data) => {
