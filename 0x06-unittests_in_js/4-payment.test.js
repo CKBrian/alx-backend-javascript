7c3d717d5727993e649195f458d51aed62fb4ab9
Brian <chumobrian@gmail.com>
2024-06-07 16:51:51 +0300

    feat: Add a test suite for the sendPaymentRequestToApi method using stubs and spies

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require('sinon');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with SUM type and correct arguments', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spyUtils = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(stubUtils.calledWith('SUM', 100, 20)).to.be.true;
+	  expect(spyUtils.calledWith('The total is: 10')).to.be.equal(true);
+    stubUtils.restore();
+  });
+});
