688e4d6374bb73132c9aaa2b262e7bc776a136f6
Brian <chumobrian@gmail.com>
2024-06-07 11:59:06 +0300

    Feat: add calculateNumber function unittest cases

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,6 +5,22 @@ describe('calculateNumber type = SUM', () => {
   it('should ADD two numbers', () => {
     assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
   });
+  it('should add two numbers: both numbers not rounded', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  });
+  it('should add two numbers: second number rounded', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+  });
+  it('should add two numbers: first number rounded', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3), 5);
+  });
+  it('should add two numbers: both numbers rounded', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+  it('should add two numbers MAX and MIN JavaScript numbers', () => {
+    assert.strictEqual(calculateNumber('SUM', Number.MAX_VALUE, Number.MIN_VALUE), Number.MAX_VALUE);
+  });
+
 });
 
 describe('calculateNumber type = SUBTRACT', () => {
